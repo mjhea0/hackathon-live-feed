a0a8501d1dc326a235aa5813031085e38363ea28
Michael Herman <hermanmu@gmail.com>
2015-02-25 17:32:38 -0700

    added commits page, route

--- server/routes/index.js
+++ server/routes/index.js
@@ -26,7 +26,44 @@ router.get('/github', ensureAuthenticated, function(req, res){
   };
 
   request(options, url, function(err, resp, body) {
+    if (err) {
+      res.status(500).send('Something broke!');
+    }
     res.status(200).send({response:body});
+  });
+
+});
+
+// todo - add helper for API call !!!!
+
+router.get('/commits', ensureAuthenticated, function(req, res){
+
+  var owner = 'RefactorU';
+  var repo = 'hackathon-live-feed';
+  var url = 'https://api.github.com/repos/'+owner+'/'+repo+'/commits';
+  var authToken = req.user.token;
+
+  var options = {
+    method: 'get',
+    json: true,
+    url: url,
+    headers : {
+      'User-Agent': 'test',
+      'Authorization': 'token '+authToken
+    }
+  };
+
+  request(options, url, function(err, resp, body) {
+    if (err) {
+      res.status(500).send('Something broke!');
+    }
+
+    response = {
+      'name': body[0].commit.author.name,
+      'date': body[0].commit.author.date,
+      'url': body[0].html_url
+    };
+    res.render('commits', response);
   });
 
 });
