47b4196dd5699fc43af4e1e39ddd52681e42f443
Michael Herman <hermanmu@gmail.com>
2015-03-01 15:59:27 -0700

    updated git commits

--- server/routes/git.js
+++ server/routes/git.js
@@ -60,7 +60,10 @@ router.get('/commits', ensureAuthenticated, function(req, res){
     for (var i = 0; i < body.length; i++) {
       responseObj.name = body[i].commit.author.name;
       responseObj.date = body[i].commit.author.date;
+      responseObj.message = body[i].commit.message;
+      responseObj.sha = body[i].sha;
       responseObj.url = body[i].html_url;
+      responseObj.avatar = body[i].author.avatar_url;
       responseArr.push(responseObj);
     }
     res.render('commits', {response:responseArr});
