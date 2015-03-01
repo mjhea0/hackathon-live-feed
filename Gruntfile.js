a8d579ace23e34ca3662524e7a6396b9dbbabadb
Chris Lizama <christopher.lizama@gmail.com>
2015-02-28 21:33:53 -0600

    jshint and live reload added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,37 @@
 module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.initConfig({
+
+    watch:{
+      markup : {
+        files:["client/views/**","client/public/js/*"], 
+        tasks:["default"],
+        options: {
+          livereload: true,
+        }
+      },
+      scripts : {
+        files:["client/public/js/*"], 
+        tasks:["jshint"]
+      }
+
+    },
+
+    jshint:{
+      files: ["client/public/js/*"],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    }
+
+  })
+
   grunt.registerTask("default", function () {
-    grunt.log.writeln("Hello world");
+    grunt.log.writeln("File change detected");
   })
 };
