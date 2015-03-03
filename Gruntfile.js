8e283983dbf84fa47034ae8797d243dbf0ff51e3
Chris Lizama <christopher.lizama@gmail.com>
2015-03-03 14:50:42 -0600

    add load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,7 @@
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks('grunt-nodemon');
-  grunt.loadNpmTasks('grunt-concurrent');
+  require('load-grunt-tasks')(grunt);
+
 
 
   grunt.initConfig({
