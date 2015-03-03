754f31911217a5f70d5cc64cdb8a658fe32e3632
Chris Lizama <christopher.lizama@gmail.com>
2015-03-03 15:20:43 -0600

    add base path to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: './',
 
 
     // frameworks to use
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        './client/public/js/main.js'
+        'client/public/js/main.js'
     ],
 
 
