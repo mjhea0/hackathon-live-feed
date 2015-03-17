e77cb5588bbb7bd183481b5f2c3420ac0687cea1
Chris Lizama <christopher.lizama@gmail.com>
2015-03-17 14:36:07 -0500

    moving toward making the component dynamic

--- client/public/js/commits.jsx
+++ client/public/js/commits.jsx
@@ -1,40 +1,54 @@
-** @jsx React.createElement */
+// ** @jsx React.createElement */
 // var React = require('react');
 
-var Commits = React.createClass({
-
- render: function () {
-   return (
-     <div>
+var Commit = React.createClass({
+  render: function () {
+    return (
+      <div>
        <p>Avatar<img></img></p>
-       <p>Name</p>
+       <p>Name{this.props.name}</p>
        <p>Date:</p>
        <p>url</p><p>sha</p>
        <p>message</p>
      </div>
-   );
- },
- loadCommentsFromServer: function() {
+      )
+  }
+})
+
+var Commits = React.createClass({
+  loadCommitsFromServer: function() {
      $.ajax({
        url: this.props.url,
        // dataType: 'json',
        success: function(data) {
-         console.log(data)
-         this.setState({data: data});
+         console.log(data.response)
+         this.setState(data.response);
        }.bind(this),
        error: function(xhr, status, err) {
          console.error(this.props.url, status, err.toString());
        }.bind(this)
      });
    },
- componentDidMount: function() {
-     this.loadCommentsFromServer();
-     setInterval(this.loadCommentsFromServer, this.props.pollInterval);
- },
-   getInitialState: function() {
-     return {data: []};
+   componentDidMount: function() {
+       this.loadCommitsFromServer();
+       setInterval(this.loadCommitsFromServer, this.props.pollInterval);
    },
-
+   getInitialState: function() {
+     return {commits: []};
+   },
+   render: function () {
+     return (
+        <div>
+          {this.state.commits.map(function (commit){
+            return (
+              <div>
+                <Commit name={commit.name}/>
+              </div>
+              )
+          })}
+        </div>
+     );
+   },
 })
 
 React.render(<Commits url="/git/fetchcommits" pollInterval={9000}></Commits>, document.getElementById('test'));
